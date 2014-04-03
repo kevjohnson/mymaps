2780a182de0ad1b33cda25e667a0832ab1aff31d
Kevin Johnson <kej.johnson@gmail.com>
2014-04-03 00:28:15 -0400

    Added code to get shapefiles

---
+++ R/getshapefiles.R
@@ -0,0 +1,42 @@
+#' Retrieves shapefile for US states
+#'
+#' This function returns a dataframe with map data for
+#' every state in the US.
+#' 
+#' @param region region to use with the fortify() function, defaults to "STATE". 
+#' Choices are "GEO_ID", "STATE", "NAME", "LSAD", "CENSUSAREA".
+#' @export
+#' @examples
+#' map <- getUS()
+#' makemap(data=d, map = map, var="obese", id="geoid", per=TRUE)
+
+getUSState <- function(region = c("STATE", "GEO_ID", "NAME", "LSAD", "CENSUSAREA")){
+    data(USState)
+    reg <- match.arg(region)
+    m <- fortify(m, region = reg)
+    return(m)
+}
+
+#' Retrieves shapefile for US counties
+#'
+#' This function returns a dataframe with map data for
+#' every county in the US or every county in a particular state.
+#' 
+#' @param state string with the full name of the state, default is NULL which
+#' gives all states.
+#' @param region region to use with the fortify() function, defaults to "GEO_ID". 
+#' Choices are "GEO_ID", "STATE", "COUNTY", "NAME", "LSAD", "CENSUSAREA".
+#' @export
+#' @examples
+#' map <- getUS()
+#' makemap(data=d, map = map, var="obese", id="geoid", per=TRUE)
+
+getUSCounty <- function(state = NULL, region = c("GEO_ID", "STATE", "COUNTY", "NAME", "LSAD", "CENSUSAREA")){
+    data(USCounty)
+    reg <- match.arg(region)
+    if (!is.null(state)){
+        m <- m[m$NAME == state,]
+    }
+    m <- fortify(m, region = reg)
+    return(m)
+}
--- R/makemap.R
+++ R/makemap.R
@@ -2,28 +2,30 @@
 #'
 #' This function takes in a dataframe, shapefile path,
 #' and variable and outputs a .png file
-
+#' 
 #' @param data dataframe with the data you want to plot
-#' @param shapefile_path string with path to the shapefile
+#' @param map dataframe with map data (after using fortify())
 #' @param var string with variable in the dataframe that you want to plot
 #' @param id string with id variable in the dataframe (e.g. "geoid")
-#' @param colors color scale to use with scale_fill_gradientn(), defaults to tim.colors(100)
+#' @param colors color scale to use with scale_fill_gradientn(), defaults 
+#' to tim.colors(100)
 #' @param title string with title of the map, defaults to blank
 #' @param fill_label string with a label for a legend, defaults to blank
 #' @param output string with output filename, defaults to "out.png"
 #' @param width defaults to 7
 #' @param height defaults to 7
 #' @param per TRUE if labels are a percentage (0.2 -> 20%), defaults to FALSE
-#' @param region string with region variable in shapefile, defaults to NULL
+#' @param projection method of projection, takes in Mercator or Albers 
+#' (Albers requires lat0 and lat1 arguments)
+#' @param proj_args any arguments necessary for the projection method
 #' @export
 #' @examples
-#' makemap(data=d, shapefile_path="data/census/shapefiles/ar/tl_2010_05_county10.shp", var="obese", id="geoid", region="COUNTYFP10", per=TRUE)
+#' makemap(data=d, map = map, var="obese", id="geoid", per=TRUE)
 
-makemap <- function(data, shapefile_path, var, id, colors = tim.colors(100),
+makeMap <- function(data, map, var, id, colors = tim.colors(100),
                     title = "", fill_label = "", output = "out.png",
-                    width = 7, height = 7, per = FALSE, region = NULL){
-    map <- readShapeSpatial(shapefile_path)
-    map <- fortify(map, region = region)
+                    width = 7, height = 7, per = FALSE,
+                    projection = c("mercator", "albers"), proj_args = NULL){
     p <- ggplot()
     p <- p + geom_map(data = data, aes_string(map_id = id, fill = var), map = map)
     p <- p + geom_path(data = map, aes(x = long, y = lat, group = group), colour = "black", size = 0.25)
@@ -32,7 +34,12 @@ makemap <- function(data, shapefile_path, var, id, colors = tim.colors(100),
     } else {
         p <- p + scale_fill_gradientn(colours = colors)
     }
-    p <- p + coord_map(projection = "mercator")
+    proj <- match.arg(projection)
+    if (proj == "mercator"){
+        p <- p + coord_map(projection = "mercator")
+    } else {
+        p <- p + coord_map(projection = "albers", lat0 = proj_args[1], lat1 = proj_args[2])
+    }
     p <- p + labs(title = title, fill = fill_label)
     p <- p + theme_nothing(legend = TRUE)
     message("Plotting Map...")
